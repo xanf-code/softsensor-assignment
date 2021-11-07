40723ae0439bf80e7b63a87b8c71a2301a3dc9b2
Darshan Aswath <65482184+xanf-code@users.noreply.github.com>
2021-11-08 01:32:03 +0530

    Update Counter.js

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { useRecoilState } from "recoil";
-import productCounter from "../Store/productCounter";
+//import { useRecoilState } from "recoil";
+//import productCounter from "../Store/productCounter";
 
 function Counter() {
-  const [count, setCount] = useRecoilState(productCounter);
+ // const [count, setCount] = useRecoilState(productCounter);
 
   return (
     <div class="flex items-center pr-4">
@@ -13,7 +13,7 @@ function Counter() {
       >
         <h1 class="text-black font-bold p-2">-</h1>
       </div>
-      <h1 class="text-black font-bold px-2">{count}</h1>
+      <h1 class="text-black font-bold px-2"> 0 </h1>
       <div
         // onClick={() => setCount(count + 1)}
         class="rounded-md border-2 bg-blue-300"
